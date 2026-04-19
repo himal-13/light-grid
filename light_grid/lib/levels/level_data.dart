04a1cbc4a4d8404378fdadb58fbd38d5cc3b14fe
himal-13 <mememimi413@gmail.com>
2026-04-19 14:23:30 +0545

    customized levels

--- light_grid/lib/levels/level_data.dart
+++ light_grid/lib/levels/level_data.dart
@@ -2,81 +2,112 @@ import 'dart:math';
 import '../models/level_model.dart';
 
 final List<LevelData> allLevels = [
-  // 3x3 Regular Squares (Levels 0-4)
-  _generateSquareLevel(0, 3, 2),
-  _generateSquareLevel(1, 3, 3),
-  _generateSquareLevel(2, 3, 4),
-  _generateSquareLevel(3, 3, 5),
-  _generateSquareLevel(4, 3, 6),
+  // // 3x3 Regular Squares (Levels 0-4)
+  // _generateSquareLevel(0, 3, 2),
+  // _generateSquareLevel(1, 3, 3),
+  // _generateSquareLevel(2, 3, 4),
+  // _generateSquareLevel(3, 3, 5),
+  // _generateSquareLevel(4, 3, 6),
 
-  // 4x4 Regular Squares (Levels 5-9)
-  _generateSquareLevel(5, 4, 3),
-  _generateSquareLevel(6, 4, 4),
-  _generateSquareLevel(7, 4, 5),
-  _generateSquareLevel(8, 4, 6),
-  _generateSquareLevel(9, 4, 8),
+  // // 4x4 Regular Squares (Levels 5-9)
+  // _generateSquareLevel(5, 4, 3),
+  // _generateSquareLevel(6, 4, 4),
+  // _generateSquareLevel(7, 4, 5),
+  // _generateSquareLevel(8, 4, 6),
+  // _generateSquareLevel(9, 4, 8),
 
   // Irregular: Cross (Levels 10-12)
   LevelData(
-    id: 10,
-    tiles: _getCrossPoints(3),
-    startingOnTiles: [const Point(0, 1), const Point(0, 2), const Point(0, 3)],
+    id: 1,
+    tiles: [
+    const Point(0, 0), const Point(1, 0), 
+  ],
+    startingOnTiles: [],
   ),
-  LevelData(
-    id: 11,
-    tiles: _getCrossPoints(5),
-    startingOnTiles: [const Point(2, 2), const Point(1, 2), const Point(3, 2), const Point(2, 1), const Point(2, 3)],
+   LevelData(
+    id: 2,
+    tiles: [
+       const Point(1, 0), const Point(2, 0),
+      const Point(0, 1), const Point(3, 1),
+      const Point(0, 2), const Point(3, 2),
+       const Point(1, 3), const Point(2, 3), 
+    ],
+    startingOnTiles: [const Point(1, 0), const Point(2, 0)],
   ),
+ 
   LevelData(
-    id: 12,
+    id: 3,
     tiles: _getCrossPoints(5),
     startingOnTiles: [const Point(0, 2), const Point(4, 2), const Point(2, 0), const Point(2, 4)],
   ),
-
+//  LevelData(
+//     id: 3,
+//     tiles: [
+//       const Point(0, 1), const Point(1, 0), const Point(1, 1), const Point(1, 2), const Point(2, 1),
+//       const Point(3, 1),
+//     ],
+//     startingOnTiles: [const Point(3, 1)],
+//   ),
   // Irregular: Diamond (Levels 13-16)
   LevelData(
-    id: 13,
+    id: 4,
     tiles: _getDiamondPoints(3),
     startingOnTiles: [const Point(1, 1)],
   ),
   LevelData(
-    id: 14,
+    id: 5,
     tiles: _getDiamondPoints(3),
     startingOnTiles: [const Point(0, 1), const Point(2, 1), const Point(1, 0), const Point(1, 2)],
   ),
   LevelData(
-    id: 15,
+    id: 6,
     tiles: _getDiamondPoints(5),
-    startingOnTiles: [const Point(2, 2), const Point(1, 2), const Point(3, 2)],
+    startingOnTiles: [const Point(0,2),const Point(2, 2), const Point(1, 2), const Point(3, 2), const Point(4, 2)],
   ),
   _generateDiamondLevel(16, 5, 5),
-
+ LevelData(
+    id: 8,
+    tiles: [
+      const Point(0, 1), const Point(1, 0), const Point(1, 1), const Point(1, 2), const Point(2, 1),
+      const Point(3, 1),
+    ],
+    startingOnTiles: [const Point(3, 1), const Point(0, 1),  ],
+  ),
   // Irregular: Triangle (Levels 17-20)
   LevelData(
-    id: 17,
+    id: 9,
     tiles: _getTrianglePoints(3),
     startingOnTiles: [const Point(0, 0), const Point(1, 1)],
   ),
   LevelData(
-    id: 18,
+    id: 10,
     tiles: _getTrianglePoints(4),
     startingOnTiles: [const Point(1, 1), const Point(2, 2)],
   ),
+  //10-11
   _generateTriangleLevel(19, 4, 4),
   _generateTriangleLevel(20, 5, 5),
 
+ LevelData(
+    id: 13,
+    tiles: [
+      const Point(0, 1), const Point(1, 0), const Point(1, 1), const Point(1, 2), const Point(2, 1), const Point(3, 1),
+      const Point(3, 2),
+    ],
+    startingOnTiles: [const Point(3, 1), const Point(0, 1),  ],
+  ),
   // 5x5 Regular Squares (Levels 21-25)
   _generateSquareLevel(21, 5, 6),
-  _generateSquareLevel(22, 5, 8),
-  _generateSquareLevel(23, 5, 10),
-  _generateSquareLevel(24, 5, 12),
-  _generateSquareLevel(25, 5, 15),
+   LevelData(
+    id: 13,
+    tiles: [
+      const Point(0, 1), const Point(1, 0), const Point(1, 1), const Point(1, 2), const Point(1, 3),
+       const Point(2, 0),  const Point(2, 1),  const Point(3, 1),  const Point(4, 1),
+      
+    ],
+    startingOnTiles: [const Point(3, 1), const Point(0, 1),  ],
+  ),
 
-  // 6x6 Regular Squares (Levels 26-29)
-  _generateSquareLevel(26, 6, 10),
-  _generateSquareLevel(27, 6, 15),
-  _generateSquareLevel(28, 6, 20),
-  _generateSquareLevel(29, 6, 25),
 ];
 
 // Helper to generate a square level with random solvable start
